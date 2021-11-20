b0546d633787905d3278b081c50a76797a46cb36
godsylla <sylvi.tran@gmail.com>
2021-11-20 12:55:52 -0800

    logisitc regression

--- src/helper.py
+++ src/helper.py
@@ -7,41 +7,54 @@ import config
 
 
 class PreProcess:
-
-    def __init__(self, X_data: pd.DataFrame, y_data: pd.DataFrame):
+    
+    def __init__(self, X_data: pd.DataFrame, verbose: bool = False):
         self.X = X_data
-        self.y = y_data
         self.pp_X = X_data.copy()
-
+        self.verbose = verbose
+        
+    @property
     def label_encode(self):
         categorical_ojbect = ['BusinessTravel', 'Department', 'EducationField', 'JobRole', 'MaritalStatus']
-        categorial_numeric = ['Education', 'EnvironmentSatisfaction', 'JobInvolvement', 'JobLevel', 
+        categorical_numeric = ['Education', 'EnvironmentSatisfaction', 'JobInvolvement', 'JobLevel', 
                               'JobSatisfaction', 'RelationshipSatisfaction', 'StockOptionLevel', 'WorkLifeBalance']
-        categorical_columns = categorical_ojbect + categorial_numeric
-        assert all(col in self.data.columns for col in categorical_columns)
+        categorical_columns = categorical_ojbect + categorical_numeric
+        assert all(col in self.X.columns for col in categorical_columns)
 
         encoders = {}
         for c in categorical_columns:
             encoders[c] = LabelEncoder()
-            encoders[c].fit(self.X[c])
-            self.pp_X[c] = encoders[c].transform(self.X[c])
+            encoders[c].fit(self.pp_X[c])
+            self.pp_X[c] = encoders[c].transform(self.pp_X[c])
             
         # need this in order to inverse transform back to the categorical labels
         self.label_encoders = encoders
+        
+        if self.verbose:
+            print(f'\nThe following categorical_columns have been label encoded: {categorical_columns}')
             
+    @property
     def binary_encode(self):
         binarizable_columns = ['Gender', 'Over18', 'OverTime']
-        assert all(col in self.data.columns for col in binarizable_columns)
+        assert all(col in self.pp_X.columns for col in binarizable_columns)
         
         encoders = {}
         for b in binarizable_columns:
             encoders[b] = LabelBinarizer()
-            encoders[b].fit(self.X[b])
-            self.pp_X[b] = encoders[b].transform(self.X[b])
+            encoders[b].fit(self.pp_X[b])
+            self.pp_X[b] = encoders[b].transform(self.pp_X[b])
             
         # need this in order to inverse transform back to the binary category labels
         self.binary_encoders = encoders
         
+        if self.verbose:
+            print(f'\nThe following categorical_columns have been binarized: {binarizable_columns}')
+        
+    def preprocess_data(self):
+        self.label_encode
+        self.binary_encode
+
+
 class IBMData:
     def __init__(self, ibm_data_filepath: str = config.DATA_PATH, verbose: bool = False):
         self.data_path = ibm_data_filepath 
@@ -75,10 +88,11 @@ class IBMData:
         self.separate_response
         
         # Preprocess prior to train-test-splitting
-        pp = PreProcess(self.X, self.y)
-        
+        pp = PreProcess(self.X, self.verbose)
+        pp.preprocess_data()
+        self.pp_X = pp.pp_X
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, random_state=config.SEED)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.pp_X, self.y, random_state=config.SEED)
 
         if self.verbose:
             print("\nTrain Test Split...")
@@ -90,5 +104,3 @@ class IBMData:
         return self.X_train, self.X_test, self.y_train, self.y_test
 
     
-
-
